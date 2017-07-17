3c513963ca6be8df823e178727035d756997360a
TammyErnst <TammyKErnst@yahoo.com>
2017-07-17 15:17:14 -0700

    repo argument

--- ghq.py
+++ ghq.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 import sys
+import argparse
 
-print "qhq"
+
+# parse arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-r", "--repo", help="GitHub Repository (ex:'h2oai/h2o-3') required", required=True)
+args = argparser.parse_args()
+
+print "*** GitHub repo " + args.repo + " ***"
